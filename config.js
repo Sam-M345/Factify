f40dcc19fde88d1e82f2e0480b8983e4a51ccca9
Sam Mirazi <61221360+Sam-M345@users.noreply.github.com>
2024-11-03 23:25:21 -0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+  supabaseUrl: "https://otxttghfmfctnyechren.supabase.co/rest/v1/facts",
+  supabaseKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90eHR0Z2hmbWZjdG55ZWNocmVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAyNjE2MzksImV4cCI6MjA0NTgzNzYzOX0.6dThND7v2ejcD8u0UIdO6NtVYfpZUvJ96uHAwvJ-1Ao",
+};
+
+export default config;
